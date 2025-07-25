dd32528324544d7fbb723a2f9bda6c6fc9c53db2
Shoaibawan005 <shoaibabbas479@gmail.com>
2025-07-25 16:11:55 +0500

    add many more features

--- SpendSmart/Controllers/HomeController.cs
+++ SpendSmart/Controllers/HomeController.cs
@@ -23,22 +23,42 @@ namespace SpendSmart.Controllers
         public IActionResult Expenses()
         {
             var allExpenses = _expensesDbContext.Expenses.ToList();
+            var totalExpense = allExpenses.Sum(x => x.Value);
+            ViewBag.Expenses = totalExpense;
             return View(allExpenses);
         }
-        public IActionResult CreateEditExpense(int id)
+        public IActionResult CreateEditExpense(int? id)
         {
-            var item = _expensesDbContext.Expenses.FirstOrDefault(x  => x.Id == id);
+            if (id != null) {
+                //editing -> load item by id
+
+                var item = _expensesDbContext.Expenses.SingleOrDefault(x => x.Id == id);
+                return View(item);
+
+            }
+
             return View();
         }
 
         public IActionResult DeleteExpense(int id)
         {
-           return RedirectToAction("Expenses");
+            var item = _expensesDbContext.Expenses.SingleOrDefault(x => x.Id == id);
+            _expensesDbContext.Expenses.Remove(item);
+            _expensesDbContext.SaveChanges();
+            return RedirectToAction("Expenses");
         }
         public IActionResult CreateEditExpenseForm(Expenses model)
         {
+            if (model.Id == 0)
+            {
+                _expensesDbContext.Expenses.Add(model);
 
-            _expensesDbContext.Expenses.Add(model);
+            }
+            else
+            {
+                _expensesDbContext.Expenses.Update(model);
+
+            }
             _expensesDbContext.SaveChanges();
             return RedirectToAction("Expenses");
         }
